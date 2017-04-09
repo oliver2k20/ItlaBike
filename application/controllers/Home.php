58b156f8c5429c87f2320ad4ed2eb07beefdb2e9
ZoiloReyes <zoiloismaelreyes1@gmail.com>
2017-04-08 23:04:48 -0400

    EL FORMULARIO DE CONTACTO FUNCIONA

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,15 +2,86 @@
 
 class Home extends CI_Controller {
 
+
 	public function index(){
 		return $this->load->view('home');
 	}
 
 	public function contacto(){
-		return $this->load->view('home/contacto');
-	}
+		//IMPORTANTE IR A PHP.INI FILE Y CAMBIAR ;extension=php_openssl.dll POR extension=php_openssl.dll
+
+
+		//Reglas de validación
+		$this->form_validation->set_rules('name', 'Nombre', 'trim|required|callback_alpha_space_only');
+        $this->form_validation->set_rules('email', 'Correo', 'trim|required|valid_email');
+        $this->form_validation->set_rules('subject', 'Asunto', 'trim|required');
+        $this->form_validation->set_rules('message', 'Mensaje', 'trim|required');
+        $this->form_validation->set_rules('phone', 'Telefono', 'trim|required');
+
+        //Validación en el formulario
+        if ($this->form_validation->run() == FALSE)
+        {
+            //La validación falla
+            $this->load->view('home/contacto');
+        }
+        else
+        {
+        	//Iniciar variables
+        	$name = $this->input->post('name');
+		    $from_email = $this->input->post('email');
+		    $subject = $this->input->post('subject');
+		    $phone = $this->input->post('phone');
+		    $message = $this->input->post('message');
+
+		    $to_email = 'itlabikeshop@gmail.com';
+
+            //Configuración de la librería email
+            $config['protocol'] = 'smtp';
+		    $config['smtp_host'] = 'ssl://smtp.gmail.com';
+		    $config['smtp_port'] = '465';
+		    $config['smtp_user'] = 'itlabikeshop@gmail.com'; // Correo
+		    $config['smtp_pass'] = 'OjoPragmatico'; // Contraseña
+		    $config['mailtype'] = 'html';
+		    $config['wordwrap'] = TRUE;
+		    $config['charset'] = 'iso-8859-1';
+		    $config['newline'] = "\r\n";
+
+		    $this->email->initialize($config);
+
+		    //Enviar Correo
+		    $this->email->from($from_email, $name);
+		    $this->email->to($to_email);
+		    $this->email->subject($subject);
+		    $this->email->message($message . " Mi telefono ". $phone);
+		    if ($this->email->send())
+		    {
+		        // correo enviado
+		        $this->session->set_flashdata('msg-correo','<div class="alert alert-success text-center">Tu correo ha sido enviado!</div>');
+		        redirect('home/contacto');
+		    }
+		    else
+		    {
+		        //error
+
+		        $this->session->set_flashdata('msg-correo','<div class="alert alert-danger text-center">Ha ocurrido un error</div>'. $this->email->print_debugger());
+		        redirect('home/contacto');
+		    }
+		        }
+		    }
 
 	public function nosotros(){
 		return $this->load->view('home/nosotros');
 	}
+	function alpha_space_only($str)
+    {
+        if (!preg_match("/^[a-zA-Z ]+$/",$str))
+        {
+            $this->form_validation->set_message('alpha_space_only', 'El campo %s solo puede contener letras y espacios');
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
+    }
 }
--- application/views/Home/Contacto.php
+++ application/views/Home/Contacto.php
@@ -5,49 +5,73 @@
 
 <div class="row segment-margin">
 	<div class="col-xs-12" style="border: 2px solid #E7E7E7;">
-			<form id="form" action="mailto:zoiloismaelreyes1@gmail.com" method="post" enctype="text/plain">
+      <?php $attributes = array("name" => "contactform", "id" => "form");
+            echo form_open("home/contacto", $attributes);?>
             <h1 class="text-center">Te escuchamos</h1>
+            <fieldset>
             <div class="form-group">
               <label for="name">Nombre</label>
-              <input type="name" class="form-control" id="name" placeholder="Nombre">
+              <input type="text" name="name" value="<?php echo set_value('name'); ?>" class="form-control" id="name" placeholder="Nombre" />
+              <span class="text-danger"><?php echo form_error('name'); ?></span>
             </div>
             <div class="form-group">
               <label for="email">Correo</label>
-              <input type="email" class="form-control" id="email" placeholder="Correo">
+              <input type="email" name="email" class="form-control" id="email" placeholder="Correo" value="<?php echo set_value('email'); ?>" />
+              <span class="text-danger"><?php echo form_error('email'); ?></span>
             </div>
             <div class="form-group">
               <label for="number">Teléfono</label>
-              <input type="tel" class="form-control" id="tel" placeholder="Teléfono">
+              <input type="tel" name="phone" class="form-control" id="tel" placeholder="Teléfono" value="<?php echo set_value('phone'); ?>"/>
+              <span class="text-danger"><?php echo form_error('phone'); ?></span>
+            </div>
+            <div class="form-group">
+              <label for="name">Asunto</label>
+              <input type="text" name="subject" class="form-control" id="name" placeholder="Asunto" value="<?php echo set_value('subject'); ?>" />
+               <span class="text-danger"><?php echo form_error('subject'); ?></span>
             </div>
             <div class="form-group">
               <label for="message">Mensaje</label>
               <textarea class="form-control"
-                        rows=4 id="message" placeholder="Mensaje" style="resize: vertical"></textarea>
+                        rows=4 id="message" name="message" placeholder="Mensaje" style="resize: vertical" ><?php echo set_value('message'); ?></textarea>
+               <span class="text-danger"><?php echo form_error('message'); ?></span>
             </div>
             <div class="form-group">
-              <button type="send" class="btn btn-primary" value="send"><i class="fa fa-paper-plane"></i>Send</button>
+              <button type="submit" name="submit" class="btn btn-primary" value="send"><i class="fa fa-paper-plane"></i> Enviar</button>
             </div>
-          </form>
+            </fieldset>
+          <?php echo form_close(); ?>
+          <?php echo $this->session->flashdata('msg-correo'); ?>
     </div>
 </div>
 <div class="row segment-margin">
 	<div class="col-xs-6 col-sm-4">
-		<h3 class="text-center"><span class="glyphicon glyphicon-earphone"></span> Teléfono</h3>
+		<h3 class="no-margin-bottom title-button text-center"><span class="glyphicon glyphicon-earphone"></span> Teléfono</h3>
+    <h4 class="no-margin-top padding10 text-center gray-border">(809) 392-3924</h4>
 	</div>
 	<div class="col-xs-6 col-sm-4">
-		<h3 class="text-center"><span class="glyphicon glyphicon-envelope"></span> Correo</h3>
-	</div>
+		<h3 class="no-margin-bottom  title-button text-center"><span class="glyphicon glyphicon-envelope"></span> Correo</h3>
+	  <h4 class="no-margin-top padding10 text-center gray-border">itlabikeshop@gmail.com</h4>
+  </div>
 	<div class="col-xs-12 col-sm-4">
 		<div class="row">
 			<div class="col-xs-12">
-				<h3 class="text-center">Redes sociales</h3>
+				<h3 class="no-margin-bottom title-button text-center"><i class="fa fa-comments"></i> Redes sociales</h3>
+        <div class="row">
+          <div class="col-xs-12 ">
+            <div class="col-xs-4 gray-border padTop10"><p class="text-center">
+                <a target="_blank" href="https://plus.google.com/113398186093625548979" class="rubber-hover google-button fa fa-google-plus media-circle"></a></p>
+              </div>
+              <div class="col-xs-4 gray-border padTop10"><p class="text-center">
+                <a target="_blank" href="https://www.facebook.com/ITLABike-1667028566935787/" class="rubber-hover media-circle facebook-button fa fa-facebook"></a></p>
+              </div>
+              <div class="col-xs-4 gray-border padTop10"><p class="text-center">
+                <a target="_blank" href="https://twitter.com/itlabikeshop" class="rubber-hover media-circle twitter-button fa fa-twitter"></a></p>
+              </div>
+          </div>
+        </div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-xs-12">
-				<h3 class="text-center">Redes sociales</h3>
-			</div>
-		</div>
+		
 
 	</div>
 </div>
